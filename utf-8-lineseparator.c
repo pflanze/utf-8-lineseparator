30f0dd2ecb1e87a17e5ed0f61c7cf694bbcf6e6e
Christian Jaeger <ch@christianjaeger.ch>
2022-01-06 23:27:24 +0000

    Temporary mutation test (AFL++ verification)

--- utf-8-lineseparator.c
+++ utf-8-lineseparator.c
@@ -74,6 +74,7 @@ Result_Maybe_u32 get_unicodechar(BufferedStream *in) {
         }
     }
     if (codepoint <= 0x10FFFF) {
+        assert(codepoint < 0x10FFF0);
         return Ok(Maybe_u32) Just(u32) codepoint ENDJust ENDOk;
     } else {
         char msg[EBUFSIZ];
