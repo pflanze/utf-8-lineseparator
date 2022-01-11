fc46065e042a19ffa58c318c02a5acb6006a78a8
Christian Jaeger <ch@christianjaeger.ch>
2022-01-11 16:56:07 +0000

    util: add yet more steps to the ladder

--- util.h
+++ util.h
@@ -49,15 +49,24 @@
     }
 
 // A hack to lead AFL towards a particular number:
-#define DEF_LADDER_FOR(var)                      \
-    int ladder_##var##_2;                        \
-    int ladder_##var##_4;                        \
-    int ladder_##var##_6;                        \
-    int ladder_##var##_8;                        \
-    int ladder_##var##_10;                       \
-    int ladder_##var##_12;                       \
-    int ladder_##var##_14;                       \
-    int ladder_##var##_16;                       \
+#define DEF_LADDER_FOR(var)                       \
+    int ladder_##var##_1;                         \
+    int ladder_##var##_2;                         \
+    int ladder_##var##_3;                         \
+    int ladder_##var##_4;                         \
+    int ladder_##var##_5;                         \
+    int ladder_##var##_6;                         \
+    int ladder_##var##_7;                         \
+    int ladder_##var##_8;                         \
+    int ladder_##var##_9;                         \
+    int ladder_##var##_10;                        \
+    int ladder_##var##_11;                        \
+    int ladder_##var##_12;                        \
+    int ladder_##var##_13;                        \
+    int ladder_##var##_14;                        \
+    int ladder_##var##_15;                        \
+    int ladder_##var##_16;                        \
+    int ladder_##var##_17;                        \
     int ladder_##var##_18;
 
 #define LADDER_UP_TO(var, from, to)                                     \
@@ -65,24 +74,48 @@
         __typeof__ (from) _from = (from);                               \
         __typeof__ (to) _to = (to);                                     \
         __typeof__ (to) _diff = _to - _from;                            \
-        if (var > (_to - (_diff >> 2))) {                               \
-            ladder_##var##_2++;                                         \
-            if (var > (_to - (_diff >> 4))) {                           \
-                ladder_##var##_4++;                                     \
-                if (var > (_to - (_diff >> 6))) {                       \
-                    ladder_##var##_6++;                                 \
-                    if (var > (_to - (_diff >> 8))) {                   \
-                        ladder_##var##_8++;                             \
-                        if (var > (_to - (_diff >> 10))) {              \
-                            ladder_##var##_10++;                        \
-                            if (var > (_to - (_diff >> 12))) {          \
-                                ladder_##var##_12++;                    \
-                                if (var > (_to - (_diff >> 14))) {      \
-                                    ladder_##var##_14++;                \
-                                    if (var > (_to - (_diff >> 16))) {  \
-                                        ladder_##var##_16++;            \
-                                        if (var > (_to - (_diff >> 18))) { \
-                                            ladder_##var##_18++;        \
+        if (var > (_to - (_diff >> 1))) {                               \
+            ladder_##var##_1++;                                         \
+            if (var > (_to - (_diff >> 2))) {                           \
+                ladder_##var##_2++;                                     \
+                if (var > (_to - (_diff >> 3))) {                       \
+                    ladder_##var##_3++;                                 \
+                    if (var > (_to - (_diff >> 4))) {                   \
+                        ladder_##var##_4++;                             \
+                        if (var > (_to - (_diff >> 5))) {              \
+                            ladder_##var##_5++;                        \
+                            if (var > (_to - (_diff >> 6))) {          \
+                                ladder_##var##_6++;                    \
+                                if (var > (_to - (_diff >> 7))) {      \
+                                    ladder_##var##_7++;                \
+                                    if (var > (_to - (_diff >> 8))) {  \
+                                        ladder_##var##_8++;            \
+                                        if (var > (_to - (_diff >> 9))) { \
+                                            ladder_##var##_9++;        \
+                                        if (var > (_to - (_diff >> 10))) { \
+                                            ladder_##var##_10++;        \
+                                        if (var > (_to - (_diff >> 11))) { \
+                                            ladder_##var##_11++;        \
+                                        if (var > (_to - (_diff >> 12))) { \
+                                            ladder_##var##_12++;        \
+                                        if (var > (_to - (_diff >> 13))) { \
+                                            ladder_##var##_13++;        \
+                                        if (var > (_to - (_diff >> 14))) { \
+                                            ladder_##var##_14++;        \
+                                        if (var > (_to - (_diff >> 15))) { \
+                                            ladder_##var##_15++;        \
+                                        if (var > (_to - (_diff >> 16))) { \
+                                            ladder_##var##_16++;        \
+                                        if (var > (_to - (_diff >> 17))) { \
+                                            ladder_##var##_17++;        \
+                                        }                               \
+                                        }                               \
+                                        }                               \
+                                        }                               \
+                                        }                               \
+                                        }                               \
+                                        }                               \
+                                        }                               \
                                         }                               \
                                     }                                   \
                                 }                                       \
