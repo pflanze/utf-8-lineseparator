a6cba3a4daef7815bb15aedc99d715dfae6aa1d9
Christian Jaeger <ch@christianjaeger.ch>
2022-01-18 09:21:08 +0000

    testinfra: add helper macros

--- testinfra.h
+++ testinfra.h
@@ -6,6 +6,8 @@
 #ifndef TESTINFRA_H_
 #define TESTINFRA_H_
 
+#include "util.h" /* XSTR */
+
 
 typedef struct {
     int successes;
@@ -24,5 +26,36 @@ static
 int teststatistics_issues(TestStatistics *stats) {
     return stats->failures + stats->errors;
 }
+
+
+// Helper macros: these expect `TestStatistics *stats` to be in scope.
+
+#define TEST_SUCCESS                            \
+    stats->successes++
+
+#define TEST_FAILURE_(fmt, ...)                 \
+    WARN_("*** Test failed: " fmt " at %s:%i",  \
+          __VA_ARGS__, __FILE__, __LINE__);     \
+    stats->failures++
+
+#define TEST_FAILURE(msg)                       \
+    TEST_FAILURE_("%s", msg)
+
+#define TEST_ERROR_(fmt, ...)                           \
+    WARN_("*** Error running test: " fmt " at %s:%i",   \
+          __VA_ARGS__, __FILE__, __LINE__);             \
+    stats->errors++
+
+#define TEST_ERROR(msg)                                 \
+    TEST_ERROR_("%s", msg)
+
+
+#define TEST_ASSERT(expr)                       \
+    if (expr) {                                 \
+        TEST_SUCCESS;                           \
+    } else {                                    \
+        TEST_FAILURE_("%s", XSTR(expr));        \
+    }
+
 
 #endif /* TESTINFRA_H_ */
